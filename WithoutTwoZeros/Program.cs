abb16a98a1c18e9b6901c084ec4e43adb2ac1ca9
MorFyNC <bulat.hisambiev@gmail.com>
2024-03-20 02:08:27 +0300

    Добавьте файлы проекта.

---
+++ WithoutTwoZeros/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(Count(2,4));
+
+int Count(int zeros, int ones)
+{
+    if (zeros > ones + 1) return 0;
+    if (zeros == 0 || ones == 0) return 1;
+    return Count(zeros, ones - 1) + Count(zeros - 1, ones - 1);
+}
